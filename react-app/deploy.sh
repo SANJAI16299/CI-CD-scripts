25794509fb61ae2d9880d6b01662b800f042f8fb
SANJAI KUMAR S <54175510+SANJAI16299@users.noreply.github.com>
2023-01-27 10:48:25 +0530

    Update deploy.sh

--- react-app/deploy.sh
+++ react-app/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -d -p 3004:80 --name react-con sanjai:v1
+cd /home/ubuntu/workspace/sample-react-app/react-app ; sudo docker run -d -p 3004:80 --name react-con sanjai:v1

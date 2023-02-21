b4d4e5699d8b633f44763c224d60ab82f9d21582
SANJAI16299 <sanjaikumar16299@gmail.com>
2023-02-21 05:40:12 +0000

    added ECR-ECS deployment files

---
+++ ECR_ECR_deployment/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/ubuntu/workspace/Sample-react-app-1/react-app/ ; sudo docker build -t sanjai:v1 .
---
+++ ECR_ECR_deployment/deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cd /home/ubuntu/workspace/Sample-react-app-1/react-app/ ; sudo docker run -d -p 3005:80 --name react-con sanjai:v1
